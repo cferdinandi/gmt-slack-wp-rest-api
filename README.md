802f9801b6b3e05bdbe7ebcb86a317cd9799dcec
cferdinandi <chris@gomakethings.com>
2018-04-18 13:42:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# GMT Slack WP Rest API
+Provides a WP Rest API endpoint for inviting users to your Slack channel.
+
+**WORK IN PROGRESS**
+
+
+## Setup
+
+1. Install the plugin using the WordPress plugin installer.
+2. Configure your settings under `Settings > Slack API` in the WordPress Dashboard.
+3. Call the desired endpoint from your code.
+
+
+## Endpoints
+
+TBD
+
+
+## License
+
+The code is available under the [GPLv3](LICENSE.md). Shoutout to [Automate Slack Invite Gravity Forms](https://wordpress.org/plugins/automate-slack-invite-gravityforms/) for providing the Slack API integration framework.
---
+++ gmt-slack-wp-rest-api.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name: GMT Slack WP Rest API
+ * Plugin URI: https://github.com/cferdinandi/gmt-slack-wp-rest-api/
+ * GitHub Plugin URI: https://github.com/cferdinandi/gmt-slack-wp-rest-api/
+ * Description: Provides a WP Rest API endpoint for inviting users to your Slack channel.
+ * Version: 0.1.0
+ * Author: Chris Ferdinandi
+ * Author URI: http://gomakethings.com
+ * License: GPLv3
+ */
+
+
+// Load includes
+if ( !class_exists( 'Slack_Invite' ) ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'includes/slack-api.php' );
+}
+require_once( plugin_dir_path( __FILE__ ) . 'includes/options.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'includes/api.php' );
